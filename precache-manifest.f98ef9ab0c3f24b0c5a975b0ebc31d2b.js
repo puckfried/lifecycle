bbd0060249a0e02a5c5544713cb9dbb86c038f6f
puckfried <puckfried@posteo.de>
2021-07-07 17:14:40 +0200

    Updates

---
+++ precache-manifest.f98ef9ab0c3f24b0c5a975b0ebc31d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3aed8d102d9db7158e2344e9bba654",
+    "url": "/puckfried/lifecycle/index.html"
+  },
+  {
+    "revision": "29ecec1f8735c9abd64d",
+    "url": "/puckfried/lifecycle/static/css/main.df6655ce.chunk.css"
+  },
+  {
+    "revision": "9c6959704ad57169a56a",
+    "url": "/puckfried/lifecycle/static/js/2.600d3970.chunk.js"
+  },
+  {
+    "revision": "29ecec1f8735c9abd64d",
+    "url": "/puckfried/lifecycle/static/js/main.abf02210.chunk.js"
+  },
+  {
+    "revision": "da27397784b5aa73a133",
+    "url": "/puckfried/lifecycle/static/js/runtime-main.c78685ea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.466d51592591398b383266d9feef9240.js"
+  "/puckfried/lifecycle/precache-manifest.f98ef9ab0c3f24b0c5a975b0ebc31d2b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puckfried/lifecycle/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
